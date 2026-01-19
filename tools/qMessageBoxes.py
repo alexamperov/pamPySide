d2d2083f5ba033a62f3c9e1c687fd94cbc17b926
alexamperov <alex.amperov@gmail.com>
2026-01-20 01:06:49 +0700

    addeed qmessagebox with notification about wrong variant chosen

--- tools/qMessageBoxes.py
+++ tools/qMessageBoxes.py
@@ -5,3 +5,7 @@ def changeVariantMessageBox(parent) -> int:
     return QMessageBox().warning( parent,"Внимание!", "Смена варианта приведет к удалению данных"
                                                 " на последующих вкладках",
                                  QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel, QMessageBox.Cancel)
+
+def wrongVariantMessageBox(parent, upperBound : int):
+    return QMessageBox().warning( parent,"Внимание!", f"Выберите вариант от 1 до {upperBound}",
+                                 QMessageBox.Ok , QMessageBox.Ok)
