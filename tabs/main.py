33ddecb14101c255af48ea43999f263b6c021edc
alexamperov <alex.amperov@gmail.com>
2026-01-20 01:05:18 +0700

    changed window title

--- tabs/main.py
+++ tabs/main.py
@@ -7,7 +7,7 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Project Creator")
+        self.setWindowTitle("Проектирование автомата Мура (реверсивный счетчик)")
         self.resize(900, 620)
 
         self.stack = QStackedWidget()
