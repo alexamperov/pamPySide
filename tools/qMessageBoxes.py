9dd015bdce0d2ec438268568b8b9952252748796
alexamperov <alex.amperov@gmail.com>
2026-01-21 22:31:55 +0700

    added message boxes for check counts triggers and states

--- tools/qMessageBoxes.py
+++ tools/qMessageBoxes.py
@@ -9,3 +9,11 @@ def changeVariantMessageBox(parent) -> int:
 def wrongVariantMessageBox(parent, upperBound : int):
     return QMessageBox().warning( parent,"Внимание!", f"Выберите вариант от 1 до {upperBound}",
                                  QMessageBox.Ok , QMessageBox.Ok)
+
+def wrongCountsCheck(parent):
+    return QMessageBox().warning( parent,"Внимание!", f"Количество состояний либо количество триггеров не соответствуют варианту!",
+                                 QMessageBox.Ok , QMessageBox.Ok)
+
+def rightCountsCheck(parent):
+    return QMessageBox().information( parent,"Внимание!", f"Количество состояний и количество триггеров соответствуют варианту!",
+                                 QMessageBox.Ok , QMessageBox.Ok)
